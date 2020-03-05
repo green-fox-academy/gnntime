30dec06822362418cd061a51572f897b704861d8
Gabriel Nagy <nagy.gabriel2@gmail.com>
2020-03-05 17:27:53 +0100

    updating exercises

---
+++ gnntime/week-01/day-04/src/MileToKmConverter.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MileToKmConverter {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input value in miles to convert:");
+        double miles = scanner.nextDouble();
+        double km = miles * 1.609344;
+        System.out.println(km + "km");
+
+
+
+    }
+}
---
+++ gnntime/week-01/day-04/src/OneTwoALot.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class OneTwoALot {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input a number");
+        int number = scanner.nextInt();
+        if (number <= 0){
+            System.out.println("Not enough!");
+
+        }
+        if (number == 1) {
+            System.out.println("Number is one");
+        }
+        if (number == 2) {
+            System.out.println("Number is two");
+        }
+        if (number > 2) {
+            System.out.println("Number is a lot");
+        }
+    }
+
+}
