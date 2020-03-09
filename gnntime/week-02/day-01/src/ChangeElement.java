d696fd0781e7102994d2b438c59cd57f808472ac
Gabriel Nagy <nagy.gabriel2@gmail.com>
2020-03-09 17:23:53 +0100

    added a day folder

---
+++ gnntime/week-02/day-01/src/ChangeElement.java
@@ -0,0 +1,13 @@
+public class ChangeElement {
+    public static void main(String[] args) {
+
+        int [] numbers = {1, 2, 3, 8, 5, 6};
+
+        int a = 8;
+        for (int i = 0; i < numbers.length ; i++) {
+        if (numbers[i] == a) {numbers[i] = 4;}
+        }
+        System.out.println(numbers[3]);
+    }
+
+}
---
+++ gnntime/week-02/day-01/src/Colors.java
@@ -0,0 +1,11 @@
+public class Colors {
+    public static void main(String[] args) {
+        String colors [] [] = {
+                {"lime" , "forest green", "olive", "pale green", "spring green"},
+                {"orange red", "red", "tomato"},
+                {"orchid", "violet", "pink", "hot pink"}
+        };
+        // test
+        System.out.println(colors[0][3]);
+    }
+}
