b1a5e494541c059aeb73d092a7c8b8910fe84de0
Gabriel Nagy <nagy.gabriel2@gmail.com>
2020-03-13 17:57:59 +0100

    commiting new code

--- Week-02/Day-03/Doubled.java
+++ Week-02/Day-03/Doubled.java
@@ -1,21 +1,45 @@
+//public class Doubled {
+//
+//    public static void main(String[] args) {
+//         Create a method that decrypts the duplicated-chars.txt
+//
+//
+//    }
+//}
+
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Doubled {
-    public static void main(String[] args) throws IOException {
-
-        Path filePath = Paths.get("files/duplicated-chars");
-        String original = Files.readString(filePath);
-
-        char[] ch = new char[original.length()];
-
-        for (int i = 0; i < original.length(); i++) {
-            ch[i] = original.charAt(i);
-        }
-
-
+    public static void main(String[] args) {
+        duplicatedDecoder("files/Duplicated.txt", "files/Duplicated_decoded.txt");
     }
 
+    public static void duplicatedDecoder(String filepathSource, String filepathDestination) {
+        Path filePathFrom = Paths.get(filepathSource);
+        Path filePathTo = Paths.get(filepathDestination);
+        try {                                                      // we decompose text to lines, lines to characters
+            // we build a parallel structure for the output
+            List<String> lines = Files.readAllLines(filePathFrom); // lines == List of strings -- so we can iterate by lines
+            List<String> linesDecoded = new ArrayList<String>(); // same for output, ArrayList is an implementation of the List interface
+            for (String line : lines) {                                     // for each line:
+                char[] characters = line.toCharArray();                     // convert the line to charArray
+                char[] charactersDecoded = new char[characters.length / 2]; // set up the same structure for the output with 1/2 length
+                for (int i = 0; i < characters.length; i += 2) {  // for each character in the line:
+                    charactersDecoded[i / 2] = characters[i];     // copy every second char from the original line to the output char-array
+                }
+                // at this point we have the decoded line in the output char-array
+                // store the line:
+                linesDecoded.add(new String(charactersDecoded)); // Files.write expects a list of strings so we need to convert the char-array to a string
+            }
+            Files.write(filePathTo, linesDecoded);
+        } catch (IOException e) {
+            System.out.println("could not read file");
+        }
+    }
 }
---
+++ Week-02/Day-05/FourRectangles.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class FourRectangles {
+
+    public static void mainDraw(Graphics graphics){
+        // draw four different size and color rectangles.
+        // avoid code duplication.
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        int x = 0;
+        int y = 0;
+        int z = 0;
+        for (int i = 0; i < 4 ; i++) {
+            graphics.drawRect(a,b,c,d);
+            graphics.setColor(new Color(x,y,z));
+        }
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
