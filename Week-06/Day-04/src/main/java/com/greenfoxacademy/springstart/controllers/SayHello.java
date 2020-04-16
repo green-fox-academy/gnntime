abc7d935b10b8ccec55b361418c413c4e41cc8d1
gnntime <35919494+gnntime@users.noreply.github.com>
2020-04-16 17:18:38 +0200

    adding sayhello exercise

---
+++ Week-06/Day-04/src/main/java/com/greenfoxacademy/springstart/controllers/SayHello.java
@@ -0,0 +1,35 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SayHello {
+
+  private AtomicLong counter = new AtomicLong();
+  private String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj",
+      "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+      "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló",
+      "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+      "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá",
+      "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+      "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem",
+      "Sawubona"};
+
+  @RequestMapping("/sayhello")
+  public String greeting(@RequestParam(name = "name") String name, @RequestParam(value = "font")
+      int fontsize, @RequestParam(value = "color") String color, Model model) {
+    String greeting = "";
+    Random random = new Random();
+    int randomNumber = random.nextInt(49);
+    greeting = hellos[randomNumber];
+    model.addAttribute("name", greeting + " " + name + "!" + " This site was loaded " +
+        counter.incrementAndGet() + " times since last server start.");
+    model.addAttribute("style", "color: " + color + ";font-size:" + fontsize + "px");
+    return "greeting2";
+  }
+}
