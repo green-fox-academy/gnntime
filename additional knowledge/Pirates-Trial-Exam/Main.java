2868799ff90bf0e35f5393f8b052817115f5b2c0
Gabriel Nagy <nagy.gabriel2@gmail.com>
2020-07-09 07:28:30 +0200

    experimenting with arrays

--- additional knowledge/Pirates-Trial-Exam/Main.java
+++ additional knowledge/Pirates-Trial-Exam/Main.java
@@ -11,6 +11,15 @@ public class Main {
     pirate4.setHasAWoodenLeg(true);
     pirate3.setHasAWoodenLeg(true);
     Ship ship1 = new Ship();
+    Ship intitializeTheShip = ship1()
+    System.out.println(ship1.getListOfpirates);
+    List <String> getListOfpirates = new ArrayList();
+    getListOfpirates = ship1
+    Iterable iterable = new Iterable;
+
+    List <data-type> list1= new ArrayList();
+    String[] strArray1 = new String[3];
+    strArray1 = ship1
     ship1.addPirate(pirate1);
     ship1.addPirate(pirate2);
     ship1.addPirate(pirate2);
@@ -25,6 +34,9 @@ public class Main {
     pirate4.work();
     pirate4.work();
     pirate4.work();
+    for (int i = 0; i < ship1 ; i++) {
+
+    }
 
     ship1.prepareForBattle();
 
